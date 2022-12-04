81e317a212908e8a26ce5810e9e62034a1313f21
Ayush Poddar <ayushpoddar158@gmail.com>
2022-12-04 17:03:31 +0530

    added date and time functionality

--- src/Component/Todos.jsx
+++ src/Component/Todos.jsx
@@ -7,26 +7,32 @@ const todos = () => {
   const [item, setItem] = useState([]);
   const [editValue, setEditValue] = useState(true);
   const[uniqueId,setUniqueId]=useState(null)
+  const [tododate,setTodoDate]=useState("")
+  const [todotime,setTodoTime]=useState("")
 
 
   // Add function
   const handleAdd = () => {
     if (input && editValue) {
-      const allInputData={id: uuidv4(),name:input}
+      const allInputData={id: uuidv4(),name:input ,date:tododate,time:todotime}
       setItem([...item, allInputData]);
       setInput("");
+      setTodoDate("")
+      setTodoTime("")
     }
     else if(input && !editValue){
      setItem(
       item.map((elem)=>{
         if(elem.id===uniqueId){
-         return {...elem,name:input}
+         return {...elem,name:input ,date:tododate,time:todotime}
         }
         return elem
 
       })
      )
      setInput("")
+     setTodoDate("")
+      setTodoTime("")
      setEditValue(true)
     }
    
@@ -53,6 +59,8 @@ const todos = () => {
   const handleEdit = (elem) => {
     setEditValue(false)
     setInput(elem.name)
+    setTodoDate(elem.tododate)
+    setTodoTime(elem.todotime)
     setUniqueId(elem.id)
   };
   return (
@@ -68,9 +76,8 @@ const todos = () => {
             }}
             placeholder="Enter your text here"
           />
-            <button className="btn addbtn">date</button>
-          <button className="btn addbtn">Time</button>
-          
+    <input value={tododate} onChange={(e)=>{setTodoDate(e.target.value)}} type="date" placeholder="Enter date" />
+    <input value={todotime} onChange={(e)=>{setTodoTime(e.target.value)}} type="Time" placeholder="Enter Time" />
           {
             editValue?  <button className="btn-primary addbtn" onClick={handleAdd}>Add </button>:<button className="btn addbtn" onClick={handleAdd}>Update</button>
           }
@@ -80,7 +87,7 @@ const todos = () => {
           {item.map((elem) => {
             return (
               <div className="eachItem" key={elem.id}>
-                <h2>{elem.name}</h2>
+                <div className="h2"><span id="elemname">{elem.name}</span> <span>{elem.date} {elem.time}</span></div>
                 <button
                   className="btn addbtn"
                   onClick={() => handleDelete(elem)}
